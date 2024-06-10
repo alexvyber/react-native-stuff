6e5c6af80ac9d55cc2e1ebfb06f8ff9f19eb8817
Alexey Sokolov <alexvyber@gmail.com>
2024-06-10 23:03:01 +0300

    init

---
+++ wa-clone/components/Bubble.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import colors from '../constants/colors';
+
+const Bubble = props => {
+    const { text, type } = props;
+
+    const bubbleStyle = { ...styles.container };
+    const textStyle = { ...styles.text };
+    const wrapperStyle = { ...styles.wrapperStyle }
+
+    switch (type) {
+        case "system":
+            textStyle.color = '#65644A';
+            bubbleStyle.backgroundColor = colors.beige;
+            bubbleStyle.alignItems = 'center';
+            bubbleStyle.marginTop = 10;
+            break;
+        case "error":
+            bubbleStyle.backgroundColor = colors.red;
+            textStyle.color = 'white';
+            bubbleStyle.marginTop = 10;
+            break;
+        case "myMessage":
+            wrapperStyle.justifyContent = 'flex-end';
+            bubbleStyle.backgroundColor = '#E7FED6';
+            bubbleStyle.maxWidth = '90%';
+            break;
+        case "theirMessage":
+            wrapperStyle.justifyContent = 'flex-start';
+            bubbleStyle.maxWidth = '90%';
+            break;
+    
+        default:
+            break;
+    }
+
+    return (
+        <View style={wrapperStyle}>
+            <View style={bubbleStyle}>
+                <Text style={textStyle}>
+                    {text}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapperStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    container: {
+        backgroundColor: 'white',
+        borderRadius: 6,
+        padding: 5,
+        marginBottom: 10,
+        borderColor: '#E2DACC',
+        borderWidth: 1
+    },
+    text: {
+        fontFamily: 'regular',
+        letterSpacing: 0.3
+    }
+})
+
+export default Bubble;
